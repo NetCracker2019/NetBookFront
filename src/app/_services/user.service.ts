18316eca930a24fd5e3e6f97cc25419d80b53cdf
Nick <48419037+pSvch0@users.noreply.github.com>
2019-11-14 23:49:33 +0200

    Update UserService

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {User} from "../_models";
+import {User} from "../_models/interface";
 import { environment } from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {throwError} from 'rxjs';
