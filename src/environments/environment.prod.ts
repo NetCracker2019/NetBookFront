871f51f6fccec301d68b8c19d1dab6cfdfdb7fc8
sashaffg <sashaffg2@gmail.com>
2019-11-22 22:08:33 +0200

    port renamed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://netbooksback.herokuapp.com'
-  apiUrl: 'http://localhost:1337'
+  apiUrl: 'https://netbooksback.herokuapp.com'
+  //apiUrl: 'http://localhost:8080'
 };
