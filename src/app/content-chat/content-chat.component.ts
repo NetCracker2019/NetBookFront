bf7cf2b61805b85fc1d7a6554460532d9bf05344
andrushechkaL <lynovskiy21@gmail.com>
2019-11-29 12:39:41 +0200

    smt

--- src/app/content-chat/content-chat.component.ts
+++ src/app/content-chat/content-chat.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {BookService} from "../_services/book.service";
+import {SubscriptionLike} from "rxjs";
 
 
 @Component({
@@ -10,16 +11,24 @@ import {BookService} from "../_services/book.service";
   templateUrl: './content-chat.component.html',
   styleUrls: ['./content-chat.component.css']
 })
-export class ContentChatComponent implements OnInit {
+export class ContentChatComponent implements OnInit, OnDestroy {
   calendarPlugins = [dayGridPlugin];
   calendarEvents: EventInput[] = [
     // { title: 'Test', date: '2019-11-11', url: '/home' },
     // { title: 'Test2', date: '2019-11-12', allDay: true }
   ];
   value: string;
+  subscription: SubscriptionLike;
 
   constructor(private bookService: BookService) {
     this.showAll();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.subscription = null;
+    }
   }
 
   ngOnInit() {
@@ -27,7 +36,7 @@ export class ContentChatComponent implements OnInit {
 
   showPersonalize() {
     this.value = 'personalize';
-    this.bookService.getCalendarAnnouncement(this.value)
+    this.subscription = this.bookService.getCalendarAnnouncement(this.value)
       .subscribe(data => { console.log(data); this.calendarEvents = data; });
   }
 
