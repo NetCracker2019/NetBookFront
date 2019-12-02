d0219588d4df25b3662281bb9c92b23dd982c268
andrushechkaL <lynovskiy21@gmail.com>
2019-12-02 16:59:26 +0200

    merge conflict1

--- src/app/content-friends/content-friends.component.ts
+++ src/app/content-friends/content-friends.component.ts
@@ -6,6 +6,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import {FormControl, FormGroup} from '@angular/forms';
 import {AuthenticationService} from '../_services/authentication.service';
 import { environment } from '../../environments/environment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-content-friends',
@@ -14,36 +15,40 @@ import { environment } from '../../environments/environment';
 })
 export class ContentFriendsComponent implements OnInit {
 
-  public peoples: User[] = [];	
+  public peoples: User[] = [];
   private login: string;
   private sought: string = "";
   public page: number = 1;
   private where: string = "friends";
-  public collectionSize: number = 0;
+  public collectionSize: number = 6;
+  public endOfFriends: boolean = false;
 
   constructor(private userService: UserService,
-   private activatedRoute: ActivatedRoute,
-   private router: Router,
-   private authenticationService: AuthenticationService,
-   private alertService: AlertService) {
-    
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService,
+              private alertService: AlertService,
+              private toastr: ToastrService) {
+
     this.login = activatedRoute.snapshot.params['login'];
     this.authenticationService.refreshToken();
-	}
+  }
 
   ngOnInit() {
     this.getPersons();
   }
-  onSearchChange(searchValue: string) {  
+  onSearchChange(searchValue: string) {
     this.page = 1;
     this.sought = searchValue;
     this.peoples = [];
+    this.endOfFriends = false;
     this.getPersons();
   }
-  onWhereChange(whereValue: string) {  
+  onWhereChange(whereValue: string) {
     this.page = 1;
     this.where = whereValue;
     this.peoples = [];
+    this.endOfFriends = false;
     this.getPersons();
   }
   onPageChanged() {
@@ -52,17 +57,22 @@ export class ContentFriendsComponent implements OnInit {
   }
 
   getPersons(){
-    this.userService.getPersons(this.login, this.sought, this.where, 6, this.page - 1)
+    this.userService.getPersons(this.login, this.sought, this.where, this.collectionSize, this.page - 1)
       .subscribe(
         (data : User[]) => {
+          if(data.length < this.collectionSize) this.endOfFriends = true;
           this.peoples = this.peoples.concat(data);
+        },
+        error => {
+          this.toastr.error(`${environment.errorMessage}`);
         });
   }
   find(){
     this.getPersons();
   }
   getPhoto(imageName: string) {
-    return `${environment.apiUrl}/files/download?filename=${imageName}`;
+    return `https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg`;
+    //return `${environment.apiUrl}/files/download?filename=${imageName}`;
   }
 
 }
