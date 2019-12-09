d5cebe4647523abda6ed88201dabdd18c65337ae
sashaffg <sashaffg2@gmail.com>
2019-12-09 23:13:23 +0200

    conflict fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://netbooksback.herokuapp.com'
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://netbooksback.herokuapp.com',
+  //apiUrl: 'http://localhost:8080',
   errorMessage: 'An unexpected error has occurred. Please contact your system administrator'
 
 };
