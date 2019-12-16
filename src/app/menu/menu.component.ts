f0eb673f541b7a677c171bea65f497f4508a7339
sashaffg <sashaffg2@gmail.com>
2019-12-17 00:35:30 +0200

    fix request issues

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Menu} from '../_models/interface';
 import {AuthenticationService} from '../_services/authentication.service';
 import {NotificationService} from "../_services/notification.service";
-import {interval} from "rxjs";
 
 
 @Component({
@@ -12,11 +11,13 @@ import {interval} from "rxjs";
 })
 export class MenuComponent implements OnInit {
 
-  public count: number ;
+  public count:number ;
   public role: number;
   public securityApprove: boolean;
   public securityAchievement: boolean;
-  public securitySuperAdmin: boolean;
+  public SuperAdmin: boolean;
+
+
 
 
   // Menu = [
@@ -42,26 +43,23 @@ export class MenuComponent implements OnInit {
 // =======
 
   constructor(private authenticationService: AuthenticationService,
-  public notificationService: NotificationService) {
-    this.role = authenticationService.currentUserValue.role;
-    this.securityApprove = this.role !== 4;
-    this.securityAchievement = this.role === 1 || this.role === 2;
-    this.securitySuperAdmin = this.role === 1;
-    if (this.authenticationService.currentUserValue.role !== 4) {
+  public notificationService:NotificationService) {
+    this.role = authenticationService.role;
+
+    this.securityApprove = this.role != 4;
+    this.securityAchievement = this.role == 1 || this.role == 2;
+    this.securitySuperAdmin = this.role == 1;
+    if(this.role != 4){
       this.Menu = this.Menu.filter(obj => obj.name !== 'My books' &&
         obj.name !== 'Chat');
     }
-  }
+
+
+   }
 
 
   ngOnInit() {
-    interval(10000)
-      .subscribe(() => {
-        this.getNotification();
-      });
-  }
 
-  getNotification() {
     this.notificationService.getCountForNotifs().subscribe(data => {
       this.count = data;
     });
@@ -73,12 +71,15 @@ export class MenuComponent implements OnInit {
     {name: 'Books', url: 'books'},
     {name: 'My profile', url: 'profile/' + this.authenticationService.currentUserValue.username},
     {name: 'Friends', url: 'friends/' + this.authenticationService.currentUserValue.username},
-    // {name: 'Notification', url: 'notifications'},
+   // {name: 'Notification', url: 'notifications'},
     {name: 'Recommendation', url: 'recommendations'},
     {name: 'My books', url: `profile/${this.authenticationService.currentUserValue.username}/book-list`},
     {name: 'Calendar', url: 'calendar'},
     {name: 'Chat', url: 'chat'},
     // {name: 'Achievements', url: 'achievements'},
     {name: 'Add book/announcement', url: 'newAnnouncement'}];
+
+    
+
 
 }
